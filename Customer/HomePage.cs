d5d0b3ad0a0d40c3997176978415d18db4209dd2
ThieuQuocHuy <fcfvbuhj2001@gmail.com>
2025-08-10 12:43:43 +0700

    first commit

---
+++ Customer/HomePage.cs
@@ -0,0 +1,35 @@
+namespace Customer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxLogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelWelcome_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
