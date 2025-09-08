71dcf0e4152ec96b11ac240c874014bf7555db61
ThieuQuocHuy <fcfvbuhj2001@gmail.com>
2025-09-08 21:08:03 +0700

    add: CSKHPage

--- Presentation/Pages/Customer/CSKHPage.Designer.cs
+++ Presentation/Pages/Customer/CSKHPage.Designer.cs
@@ -28,12 +28,326 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CSKHPage";
+            panelSidebar = new Panel();
+            buttonCSKH = new Button();
+            pictureBoxLogo = new PictureBox();
+            buttonHome = new Button();
+            buttonProducts = new Button();
+            buttonCart = new Button();
+            buttonOrders = new Button();
+            buttonLogout = new Button();
+            pictureBoxMain = new PictureBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            labelTitle = new Label();
+            dataGridRequests = new DataGridView();
+            labelSendTitle = new Label();
+            textSupportContent = new TextBox();
+            buttonSend = new Button();
+            panelSidebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMain).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridRequests).BeginInit();
+            SuspendLayout();
+            // 
+            // panelSidebar
+            // 
+            panelSidebar.BackColor = Color.FromArgb(77, 58, 41);
+            panelSidebar.Controls.Add(buttonCSKH);
+            panelSidebar.Controls.Add(pictureBoxLogo);
+            panelSidebar.Controls.Add(buttonHome);
+            panelSidebar.Controls.Add(buttonProducts);
+            panelSidebar.Controls.Add(buttonCart);
+            panelSidebar.Controls.Add(buttonOrders);
+            panelSidebar.Controls.Add(buttonLogout);
+            panelSidebar.Dock = DockStyle.Left;
+            panelSidebar.Location = new Point(0, 0);
+            panelSidebar.Name = "panelSidebar";
+            panelSidebar.Size = new Size(200, 593);
+            panelSidebar.TabIndex = 0;
+            // 
+            // buttonCSKH
+            // 
+            buttonCSKH.BackColor = Color.FromArgb(221, 207, 182);
+            buttonCSKH.FlatStyle = FlatStyle.Flat;
+            buttonCSKH.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonCSKH.ForeColor = Color.White;
+            buttonCSKH.Location = new Point(0, 380);
+            buttonCSKH.Name = "buttonCSKH";
+            buttonCSKH.Padding = new Padding(30, 0, 0, 0);
+            buttonCSKH.Size = new Size(200, 50);
+            buttonCSKH.TabIndex = 6;
+            buttonCSKH.Text = "CSKH";
+            buttonCSKH.TextAlign = ContentAlignment.MiddleLeft;
+            buttonCSKH.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.Location = new Point(0, 0);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.Size = new Size(200, 182);
+            pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxLogo.TabIndex = 0;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // buttonHome
+            // 
+            buttonHome.BackColor = Color.FromArgb(77, 58, 41);
+            buttonHome.FlatAppearance.BorderSize = 0;
+            buttonHome.FlatStyle = FlatStyle.Flat;
+            buttonHome.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonHome.ForeColor = Color.White;
+            buttonHome.Location = new Point(0, 180);
+            buttonHome.Name = "buttonHome";
+            buttonHome.Padding = new Padding(30, 0, 0, 0);
+            buttonHome.Size = new Size(200, 50);
+            buttonHome.TabIndex = 1;
+            buttonHome.Text = "Trang chủ";
+            buttonHome.TextAlign = ContentAlignment.MiddleLeft;
+            buttonHome.UseVisualStyleBackColor = false;
+            // 
+            // buttonProducts
+            // 
+            buttonProducts.BackColor = Color.FromArgb(77, 58, 41);
+            buttonProducts.FlatAppearance.BorderSize = 0;
+            buttonProducts.FlatStyle = FlatStyle.Flat;
+            buttonProducts.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonProducts.ForeColor = Color.White;
+            buttonProducts.Location = new Point(0, 230);
+            buttonProducts.Name = "buttonProducts";
+            buttonProducts.Padding = new Padding(30, 0, 0, 0);
+            buttonProducts.Size = new Size(200, 50);
+            buttonProducts.TabIndex = 2;
+            buttonProducts.Text = "Sản phẩm";
+            buttonProducts.TextAlign = ContentAlignment.MiddleLeft;
+            buttonProducts.UseVisualStyleBackColor = false;
+            // 
+            // buttonCart
+            // 
+            buttonCart.BackColor = Color.FromArgb(77, 58, 41);
+            buttonCart.FlatAppearance.BorderSize = 0;
+            buttonCart.FlatStyle = FlatStyle.Flat;
+            buttonCart.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonCart.ForeColor = Color.White;
+            buttonCart.Location = new Point(0, 280);
+            buttonCart.Name = "buttonCart";
+            buttonCart.Padding = new Padding(30, 0, 0, 0);
+            buttonCart.Size = new Size(200, 50);
+            buttonCart.TabIndex = 3;
+            buttonCart.Text = "Giỏ hàng";
+            buttonCart.TextAlign = ContentAlignment.MiddleLeft;
+            buttonCart.UseVisualStyleBackColor = false;
+            // 
+            // buttonOrders
+            // 
+            buttonOrders.BackColor = Color.FromArgb(77, 58, 41);
+            buttonOrders.FlatAppearance.BorderSize = 0;
+            buttonOrders.FlatStyle = FlatStyle.Flat;
+            buttonOrders.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonOrders.ForeColor = Color.White;
+            buttonOrders.Location = new Point(0, 330);
+            buttonOrders.Name = "buttonOrders";
+            buttonOrders.Padding = new Padding(30, 0, 0, 0);
+            buttonOrders.Size = new Size(200, 50);
+            buttonOrders.TabIndex = 4;
+            buttonOrders.Text = "Đơn mua";
+            buttonOrders.TextAlign = ContentAlignment.MiddleLeft;
+            buttonOrders.UseVisualStyleBackColor = false;
+            // 
+            // buttonLogout
+            // 
+            buttonLogout.BackColor = Color.FromArgb(120, 40, 40);
+            buttonLogout.FlatAppearance.BorderSize = 0;
+            buttonLogout.FlatStyle = FlatStyle.Flat;
+            buttonLogout.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            buttonLogout.ForeColor = Color.White;
+            buttonLogout.Location = new Point(0, 430);
+            buttonLogout.Name = "buttonLogout";
+            buttonLogout.Padding = new Padding(30, 0, 0, 0);
+            buttonLogout.Size = new Size(200, 50);
+            buttonLogout.TabIndex = 5;
+            buttonLogout.Text = "Đăng xuất";
+            buttonLogout.TextAlign = ContentAlignment.MiddleLeft;
+            buttonLogout.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxMain
+            // 
+            pictureBoxMain.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxMain.Location = new Point(200, 0);
+            pictureBoxMain.Margin = new Padding(0);
+            pictureBoxMain.Name = "pictureBoxMain";
+            pictureBoxMain.Size = new Size(963, 46);
+            pictureBoxMain.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxMain.TabIndex = 1;
+            pictureBoxMain.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(237, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(277, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Cửa hàng HeartSteelComestic";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(237, 224, 207);
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(200, 45);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(963, 105);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(237, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(460, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Chào mừng bạn đã đến với cửa hàng của chúng tôi!";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(237, 105);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Khách hàng:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(377, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(155, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Thiều Quốc Huy";
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelTitle.ForeColor = Color.FromArgb(77, 58, 41);
+            labelTitle.Location = new Point(210, 168);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(304, 29);
+            labelTitle.TabIndex = 7;
+            labelTitle.Text = "Danh sách yêu cầu hỗ trợ";
+            labelTitle.Click += labelTitle_Click;
+            // 
+            // dataGridRequests
+            // 
+            dataGridRequests.AllowUserToAddRows = false;
+            dataGridRequests.AllowUserToDeleteRows = false;
+            dataGridRequests.BackgroundColor = Color.White;
+            dataGridRequests.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridRequests.Location = new Point(217, 200);
+            dataGridRequests.Name = "dataGridRequests";
+            dataGridRequests.ReadOnly = true;
+            dataGridRequests.RowHeadersVisible = false;
+            dataGridRequests.RowHeadersWidth = 51;
+            dataGridRequests.Size = new Size(882, 141);
+            dataGridRequests.TabIndex = 8;
+            dataGridRequests.CellContentClick += dataGridRequests_CellContentClick;
+            // 
+            // labelSendTitle
+            // 
+            labelSendTitle.AutoSize = true;
+            labelSendTitle.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelSendTitle.ForeColor = Color.FromArgb(77, 58, 41);
+            labelSendTitle.Location = new Point(217, 355);
+            labelSendTitle.Name = "labelSendTitle";
+            labelSendTitle.Size = new Size(188, 25);
+            labelSendTitle.TabIndex = 9;
+            labelSendTitle.Text = "Gửi yêu cầu hỗ trợ";
+            // 
+            // textSupportContent
+            // 
+            textSupportContent.Location = new Point(217, 394);
+            textSupportContent.Multiline = true;
+            textSupportContent.Name = "textSupportContent";
+            textSupportContent.Size = new Size(869, 100);
+            textSupportContent.TabIndex = 10;
+            // 
+            // buttonSend
+            // 
+            buttonSend.BackColor = Color.FromArgb(95, 76, 59);
+            buttonSend.FlatAppearance.BorderSize = 0;
+            buttonSend.FlatStyle = FlatStyle.Flat;
+            buttonSend.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            buttonSend.ForeColor = Color.White;
+            buttonSend.Location = new Point(923, 522);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new Size(195, 47);
+            buttonSend.TabIndex = 11;
+            buttonSend.Text = "📩 Gửi yêu cầu";
+            buttonSend.UseVisualStyleBackColor = false;
+            // 
+            // CSKHPage
+            // 
+            BackColor = Color.FromArgb(237, 224, 207);
+            ClientSize = new Size(1142, 593);
+            Controls.Add(buttonSend);
+            Controls.Add(textSupportContent);
+            Controls.Add(labelSendTitle);
+            Controls.Add(dataGridRequests);
+            Controls.Add(labelTitle);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBoxMain);
+            Controls.Add(panelSidebar);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MaximumSize = new Size(1160, 640);
+            MinimumSize = new Size(1160, 640);
+            Name = "CSKHPage";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CSKH";
+            panelSidebar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMain).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridRequests).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Panel panelSidebar;
+		private PictureBox pictureBoxLogo;
+		private Button buttonHome;
+		private Button buttonProducts;
+		private Button buttonCart;
+		private Button buttonOrders;
+		private Button buttonLogout;
+		private Button buttonCSKH;
+		private PictureBox pictureBoxMain;
+		private Label label1;
+		private PictureBox pictureBox1;
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private Label labelTitle;
+		private DataGridView dataGridRequests;
+		private Label labelSendTitle;
+		private TextBox textSupportContent;
+		private Button buttonSend;
     }
 }
--- Presentation/Pages/Customer/CSKHPage.cs
+++ Presentation/Pages/Customer/CSKHPage.cs
@@ -15,6 +15,73 @@ namespace Presentation.Pages.Customer
         public CSKHPage()
         {
             InitializeComponent();
+            // Hiển thị tên người dùng đang đăng nhập
+            label4.Text = Presentation.Auth.UserSession.CurrentUser?.Fullname ?? label4.Text;
+            // Gán sự kiện cho các nút điều hướng
+            buttonHome.Click += (_, __) => Presentation.Navigation.Navigator.Navigate(new HomePage());
+            buttonProducts.Click += (_, __) => Presentation.Navigation.Navigator.Navigate(new ProductPage());
+            buttonCart.Click += (_, __) => Presentation.Navigation.Navigator.Navigate(new CartPage());
+            buttonOrders.Click += (_, __) => Presentation.Navigation.Navigator.Navigate(new OrderListPage());
+            buttonLogout.Click += (_, __) => { Presentation.Auth.UserSession.Clear(); Presentation.Navigation.Navigator.Navigate(new LoginForm()); };
+            buttonSend.Click += ButtonSend_Click;
+            // Load ảnh logo nếu có
+            LoadImages();
+            // Cấu hình bảng danh sách
+            ConfigureGrid();
+        }
+
+        private void LoadImages()
+        {
+            try
+            {
+                var logoImage = Presentation.Services.ResourceImageLoader.LoadByFileName("logoden.png");
+                if (logoImage != null)
+                {
+                    pictureBoxLogo.Image = logoImage;
+                }
+            }
+            catch { }
+        }
+
+        private void ConfigureGrid()
+        {
+            if (dataGridRequests.Columns.Count == 0)
+            {
+                dataGridRequests.Columns.Add("Id", "ID yêu cầu");
+                dataGridRequests.Columns.Add("Customer", "Khách hàng");
+                dataGridRequests.Columns.Add("Phone", "Số điện thoại");
+                dataGridRequests.Columns.Add("Content", "Nội dung yêu cầu");
+                dataGridRequests.Columns.Add("CreatedAt", "Ngày gửi");
+            }
+        }
+
+        private void ButtonSend_Click(object? sender, EventArgs e)
+        {
+            var content = textSupportContent.Text.Trim();
+            if (string.IsNullOrEmpty(content))
+            {
+                MessageBox.Show("Vui lòng nhập nội dung yêu cầu.");
+                return;
+            }
+            // Hiện tại chỉ demo thêm vào bảng hiển thị
+            dataGridRequests.Rows.Add(
+                dataGridRequests.Rows.Count + 1,
+                Presentation.Auth.UserSession.CurrentUser?.Fullname ?? "Khách",
+                Presentation.Auth.UserSession.CurrentUser?.Phone ?? "",
+                content,
+                DateTime.Now.ToString("dd/MM/yyyy HH:mm"));
+            textSupportContent.Clear();
+            MessageBox.Show("Đã gửi yêu cầu hỗ trợ!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void labelTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridRequests_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
